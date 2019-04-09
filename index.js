73a0e060b5d924ae6a5fd4682572f4aaa91c99fa
incendiary-coder-8761 <ken.zh77@gmail.com>
2019-04-09 02:32:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,42 +2,6 @@ function shout(string){
   return string.toUpperCase();
 }
 
-
-function whisper(string){
-  return string.toLowerCase();
-}
-
-
-//function logShout(string){
-// var spy = string.toUpperCase();
-//  console.log(`${spy}`);
-//}
-
-function logWhisper(string){
-  var spy = string.toLowerCase();
-  console.log(`${spy}`);
-}
-
-function sayHiToGrandma(string){
-  var spy = string;
-  if (spy.toLowerCase() === spy){
-    return "I can't hear you!";
-  }else if (spy === "I love you, Grandma.") {
-    return "I love you, too.";
-  }else{
-    return "YES INDEED!";
-  }
-}
-
-
-
-
-
-
-function shout(string){
-  return string.toUpperCase();
-}
-
 function whisper(string){
   return string.toLowerCase();
 }
