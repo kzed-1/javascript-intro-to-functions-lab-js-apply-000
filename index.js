b5756e437db9c75344db2e7244a7205f976f8a8d
incendiary-coder-8761 <ken.zh77@gmail.com>
2019-04-09 02:32:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,5 +53,13 @@ function logWhisper(string){
 
 
 function sayHiToGrandma(string){
-  
+  if (string === string.toLowerCase()){
+    return "I can't hear you!";
+  } 
+  else if (string === "I love you, Grandma."){
+    return "I love you, too.";
+  }
+  else{
+    return "YES INDEED!";
+  }
 }
